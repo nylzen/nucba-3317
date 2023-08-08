64a36f0d1c869b7d68e62cf99d26231975e37822
Nelson <tugoresn@gmail.com>
2023-08-08 20:36:09 -0300

    Javascript Clase 1

---
+++ Javascript/clase_01/main.js
@@ -0,0 +1,19 @@
+// alert("ESTO ES JS");
+
+/* 
+ Tipos de datos en JS:
+ - Number: 5, 6.7, 8.9, 9.0
+ - String: "Hola", 'Hola', `Hola`
+ - Boolean: true, false
+ - undefined
+ - null
+*/
+
+// Variables
+
+// var nombreVariable = lo que quiera guardar
+var numeroA = 5;
+var numeroB = 10;
+var resultado = numeroA + numeroB;
+
+console.log(resultado); // 15
