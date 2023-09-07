ee1888e90e60418717e89d5cf5f3845cb465e5db
Nelson <tugoresn@gmail.com>
2023-09-07 19:50:32 -0300

    add consignas

--- Javascript/clase_09-repaso-metodos/main.js
+++ Javascript/clase_09-repaso-metodos/main.js
@@ -70,3 +70,42 @@ const productos = [
     categorias: ["Hogar", "Cocina"],
   },
 ];
+
+// 1. **Filtrar productos por categoría:**
+// Filtra los productos para obtener solo aquellos que pertenecen a la categoría "Ropa".
+
+// 2. Usando destructuring traer el nombre de los productos en un nuevo array
+
+// 3. **Encontrar productos agotados:**
+// Encuentra y muestra los productos que tienen un stock igual a 0.
+
+// 4. **Calcular el valor total del inventario:**
+// Calcula el valor total del inventario sumando el precio de todos los productos en stock.
+
+// 5. **Encontrar productos de tecnología:**
+// Encuentra y muestra los productos que pertenecen a la categoría "Tecnología".
+
+// 6. **Ordenar productos por precio:**
+// Ordena los productos por precio de menor a mayor.
+
+// 7. **Modificar el precio de todos los productos:**
+// Aumenta el precio de todos los productos en un 10%.
+
+// 8. **Encontrar productos que pertenecen a múltiples categorías:**
+// Encuentra y muestra los productos que pertenecen a más de una categoría (por ejemplo, "Electrónica" y "Tecnología").
+
+// 9. **Encontrar el producto más caro:**
+// Encuentra el producto con el precio más alto.
+
+// 10. **Filtrar productos por precio:**
+// Filtra los productos para obtener aquellos que tienen un precio menor o igual a $30.
+
+// 11.  Encontrar productos con nombres largos (más de 30 caracteres)
+
+// 12. Encontrar productos con stock mínimo (menor o igual a 10 unidades)
+
+// 13. Hacer una función que devuelva un nuevo array con los productos actuales pero que aquellos que valgan mas de $300 tengan un descuento del 50%
+
+// 14. Calcular el promedio de precios de productos en la categoría "Ropa”
+
+// Imprimir por consola (por cada producto) : "El producto *nombre* sale $*precio*"
