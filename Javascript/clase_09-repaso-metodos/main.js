ea939a742fdfc0515c80d8ff0bb44adaabbfaf81
Nelson <tugoresn@gmail.com>
2023-09-07 21:45:37 -0300

    Clase 09 Repaso metodos array

--- Javascript/clase_09-repaso-metodos/main.js
+++ Javascript/clase_09-repaso-metodos/main.js
@@ -74,38 +74,131 @@ const productos = [
 // 1. **Filtrar productos por categoría:**
 // Filtra los productos para obtener solo aquellos que pertenecen a la categoría "Ropa".
 
+const productosRopa = productos.filter((producto) =>
+  producto.categorias.includes("Ropa")
+);
+
+// console.log(productosRopa);
+
 // 2. Usando destructuring traer el nombre de los productos en un nuevo array
+
+// const nombreProductos = productos.map((producto) => producto.nombre);
+const nombreProductos = productos.map(({ nombre }) => nombre);
+// console.log(nombreProductos);
 
 // 3. **Encontrar productos agotados:**
 // Encuentra y muestra los productos que tienen un stock igual a 0.
 
+const productosAgotados = productos.filter((producto) => producto.stock === 0);
+// console.log(productosAgotados);
+
 // 4. **Calcular el valor total del inventario:**
 // Calcula el valor total del inventario sumando el precio de todos los productos en stock.
+
+const valorTotalInventario = productos.reduce(
+  (total, producto) => total + producto.precio * producto.stock,
+  0
+);
+// console.log(`El valor total del inventario es de $${valorTotalInventario}`);
+// console.log(valorTotalInventario);
 
 // 5. **Encontrar productos de tecnología:**
 // Encuentra y muestra los productos que pertenecen a la categoría "Tecnología".
 
+const filtrarProductos = (array, categoria) => {
+  return array.filter((producto) => {
+    return producto.categorias.includes(categoria);
+  });
+};
+
+// console.log(filtrarProductos(productos, "Tecnología"));
+
+const productosTecnologia = productos.filter((producto) =>
+  producto.categorias.includes("Tecnología")
+);
+// console.log(productosTecnologia);
+
 // 6. **Ordenar productos por precio:**
 // Ordena los productos por precio de menor a mayor.
 
+// const productosMenorAMayor = productos.sort((a, b) => a.precio - b.precio);
+// console.log(productosMenorAMayor);
+
 // 7. **Modificar el precio de todos los productos:**
 // Aumenta el precio de todos los productos en un 10%.
+const productosInflados = productos.map((producto) => ({
+  ...producto,
+  precioInflado: producto.precio * 1.1,
+}));
+
+// console.log(productosInflados);
 
 // 8. **Encontrar productos que pertenecen a múltiples categorías:**
 // Encuentra y muestra los productos que pertenecen a más de una categoría (por ejemplo, "Electrónica" y "Tecnología").
 
+const multipleCategoria = productos.filter(
+  (producto) => producto.categorias.length > 1
+);
+
+// console.log(multipleCategoria);
+
 // 9. **Encontrar el producto más caro:**
 // Encuentra el producto con el precio más alto.
+
+const productoMasCaro = productos.reduce((productoMasCaro, producto) =>
+  producto.precio > productoMasCaro.precio ? producto : productoMasCaro
+);
+
+// console.log(productoMasCaro);
 
 // 10. **Filtrar productos por precio:**
 // Filtra los productos para obtener aquellos que tienen un precio menor o igual a $30.
 
-// 11.  Encontrar productos con nombres largos (más de 30 caracteres)
+const productoMenorIgual30 = productos.filter(
+  (producto) => producto.precio <= 30
+);
+// console.log(productoMenorIgual30);
 
+// 11.  Encontrar productos con nombres largos (más de 30 caracteres)
+const productosMasDe30 = productos
+  .filter(({ nombre }) => nombre.length > 10)
+  .slice(-5)
+  .map((producto) => {
+    return `El producto mas largo es ${producto.nombre}`;
+  });
+// console.log(productosMasDe30);
 // 12. Encontrar productos con stock mínimo (menor o igual a 10 unidades)
 
+const productoStockMinimo = productos.filter(
+  (producto) => producto.stock <= 10
+);
+// console.log(productoStockMinimo);
+
 // 13. Hacer una función que devuelva un nuevo array con los productos actuales pero que aquellos que valgan mas de $300 tengan un descuento del 50%
+const productosConDescuento = productos.map((producto) =>
+  producto.precio > 300
+    ? { ...producto, precio: producto.precio * 1.5 }
+    : producto
+);
+
+// console.log(productosConDescuento);
 
 // 14. Calcular el promedio de precios de productos en la categoría "Ropa”
 
+const productosRopa2 = productos.filter((producto) =>
+  producto.categorias.includes("Ropa")
+);
+// console.log(productosRopa2);
+const promedioPrecioRopa =
+  productosRopa2.reduce((total, producto) => total + producto.precio, 0) /
+  productosRopa2.length;
+
+// console.log(promedioPrecioRopa);
+
 // Imprimir por consola (por cada producto) : "El producto *nombre* sale $*precio*"
+
+const imprimirNombrePrecio = productos.forEach((producto) =>
+  console.log(`El producto ${producto.nombre} sale $${producto.precio}`)
+);
+
+imprimirNombrePrecio;
