040320a45cc5da3db03c31efb479b808f0d46c69
Nelson <tugoresn@gmail.com>
2023-10-12 23:19:26 -0300

    Clase 18: Nucba NFT II

--- Javascript/clase_017_NucbaNFT/assets/index.js
+++ Javascript/clase_017_NucbaNFT/assets/index.js
@@ -6,9 +6,31 @@ const showMoreBtn = document.querySelector(".btn-load");
 const categoriesContainer = document.querySelector(".categories");
 // El HTML Collection de todas las categorias
 const categoriesList = document.querySelectorAll(".category");
+// Carrito
+const cartBtn = document.querySelector(".cart-label");
+// Boton para abrir y cerrar el menu
+const menuBtn = document.querySelector(".menu-label");
+// Carrito div
+const cartMenu = document.querySelector(".cart");
+// Menu (Hamburguesa)
+const barsMenu = document.querySelector(".navbar-list");
+// Overlay
+const overlay = document.querySelector(".overlay");
+// Cart bubble
+const cartBubble = document.querySelector(".cart-bubble");
+// Total del carrito
+const total = document.querySelector(".total");
+// Boton de comprar
+const buyBtn = document.querySelector(".btn-buy");
+// Boton para borrar
+const deleteBtn = document.querySelector(".btn-delete");
+// Cart container
+const productsCart = document.querySelector(".cart-container");
+
+// Seteamos el carrito
+let cart = [];
 
 /* --------- Logica reder products ---------- */
-
 // Funcion para crear el html del producto
 // TODO: AGREGAR DATASETS AL BUTTON
 const createProductTemplate = (product) => {
@@ -40,7 +62,12 @@ const createProductTemplate = (product) => {
           <p>05:12:07</p>
         </div>
     
-        <button class="btn-add">Add</button>
+        <button class="btn-add"
+        data-id='${id}' 
+        data-name='${name}'
+        data-bid='${bid}' 
+        data-img='${cardImg}'
+        >Add</button>
       </div>
     </div>
     </div>`;
@@ -134,11 +161,109 @@ const isInactiveFilterBtn = (element) => {
   );
 };
 
+/* --------- Logica Menu/Carrito Modal ---------- */
+const toggleCart = () => {
+  cartMenu.classList.toggle("open-cart");
+
+  if (barsMenu.classList.contains("open-menu")) {
+    barsMenu.classList.remove("open-menu");
+    return;
+  }
+
+  overlay.classList.toggle("show-overlay");
+};
+
+const toggleMenu = () => {
+  barsMenu.classList.toggle("open-menu");
+
+  if (cartMenu.classList.contains("open-cart")) {
+    cartMenu.classList.remove("open-cart");
+    return;
+  }
+  overlay.classList.toggle("show-overlay");
+};
+
+// Funcion para cerrar el menu cuando se clickea el overlay
+const closeOnOverlayClick = () => {
+  barsMenu.classList.remove("open-menu");
+  cartMenu.classList.remove("open-cart");
+  overlay.classList.remove("show-overlay");
+};
+
+// Funcion para cerrar menues, cuando scrolleamos
+const closeOnScroll = () => {
+  if (
+    barsMenu.classList.contains("open-menu") &&
+    cartMenu.classList.contains("open-cart")
+  ) {
+    return;
+  }
+
+  barsMenu.classList.remove("open-menu");
+  cartMenu.classList.remove("open-cart");
+  overlay.classList.remove("show-overlay");
+};
+
+/* --------- --------------- ---------- */
+/* --------- Logica carrito  ---------- */
+/* --------- --------------- ---------- */
+// Render carrito
+const renderCart = () => {
+  if (!cart.length) {
+    productsCart.innerHTML = `<p class="empty-msg">Agrega un producto raton</p>`;
+    return;
+  }
+
+  alert("tuki");
+};
+
+const addProduct = (e) => {
+  if (!e.target.classList.contains("btn-add")) return;
+  const product = e.target.dataset;
+
+  // Tendriamos que hacer un if para verificar que el producto a agregar no este en el carrito
+  if (isExistingCartProduct(product)) {
+    addUnitToProduct(product);
+  } else {
+    createCartProduct(product);
+  }
+
+  renderCart();
+  console.log(cart);
+};
+
+// Funcion para agregar una unidad al producto
+const addUnitToProduct = (product) => {
+  cart = cart.map((cartProduct) =>
+    cartProduct.id === product.id
+      ? { ...cartProduct, quantity: cartProduct.quantity + 1 }
+      : cartProduct
+  );
+};
+
+// Funcion para saber si un producto ya existe en el carrito
+const isExistingCartProduct = (product) => {
+  return cart.find((item) => item.id === product.id);
+};
+
+// Funcion para crear un objeto con la info del produ que queremos agregar al carrito
+const createCartProduct = (product) => {
+  cart = [...cart, { ...product, quantity: 1 }];
+};
+
 // Funcion init
 const init = () => {
   renderProducts(appState.products[0]);
   showMoreBtn.addEventListener("click", showMoreProducts);
   categoriesContainer.addEventListener("click", applyFilter);
+
+  cartBtn.addEventListener("click", toggleCart);
+  menuBtn.addEventListener("click", toggleMenu);
+  overlay.addEventListener("click", closeOnOverlayClick);
+  window.addEventListener("scroll", closeOnScroll);
+
+  productsContainer.addEventListener("click", addProduct);
+  document.addEventListener("DOMContentLoaded", renderCart);
 };
 
 init();
